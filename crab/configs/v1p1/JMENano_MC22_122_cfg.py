0c13b144f2ce0599d3218a90a5cbb1ead39964c3
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2022-09-05 17:32:55 +0200

    Updates for v1p1

---
+++ crab/configs/v1p1/JMENano_MC22_122_cfg.py
@@ -0,0 +1,127 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step1 --filein root://xrootd-cms.infn.it//store/mc/Run3Winter22MiniAOD/DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/MINIAODSIM/122X_mcRun3_2021_realistic_v9_ext2-v2/40000/72dd806e-eef1-4387-b08c-03836e178303.root --fileout root://eosuser.cern.ch//eos/user/n/nbinnorj/JMENano/Run3/JMENano_MC22_122_DYJetsToLL.root --python_filename ./OUTPUT/JMENano_MC22_122_DYJetsToLL_cfg.py --step NANO --conditions 122X_mcRun3_2021_realistic_v9 --era Run3,run3_nanoAOD_122 --mc --eventcontent NANOAODSIM --datatier NANOAODSIM -n 100 --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring --customise_commands=process.add_(cms.Service('InitRootHandlers', EnableIMT = cms.untracked.bool(False)))\nprocess.MessageLogger.cerr.FwkReport.reportEvery = 10\nfrom PhysicsTools.NanoAOD.custom_jme_cff import PrepJMECustomNanoAOD_MC; PrepJMECustomNanoAOD_MC(process)\n
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run3_cff import Run3
+from Configuration.Eras.Modifier_run3_nanoAOD_122_cff import run3_nanoAOD_122
+
+process = cms.Process('NANO',Run3,run3_nanoAOD_122)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('PhysicsTools.NanoAOD.nano_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1),
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('root://xrootd-cms.infn.it//store/mc/Run3Winter22MiniAOD/DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/MINIAODSIM/122X_mcRun3_2021_realistic_v9_ext2-v2/40000/72dd806e-eef1-4387-b08c-03836e178303.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    SkipEvent = cms.untracked.vstring(),
+    accelerators = cms.untracked.vstring('*'),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
+    dumpOptions = cms.untracked.bool(False),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+            allowAnyLabel_=cms.required.untracked.uint32
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(0)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(0),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step1 nevts:-1'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.NANOAODSIMoutput = cms.OutputModule("NanoAODOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(9),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('NANOAODSIM'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:tree.root'),
+    outputCommands = process.NANOAODSIMEventContent.outputCommands
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '122X_mcRun3_2021_realistic_v9', '')
+
+# Path and EndPath definitions
+process.nanoAOD_step = cms.Path(process.nanoSequenceMC)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.NANOAODSIMoutput_step = cms.EndPath(process.NANOAODSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.nanoAOD_step,process.endjob_step,process.NANOAODSIMoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from PhysicsTools.NanoAOD.nano_cff
+from PhysicsTools.NanoAOD.nano_cff import nanoAOD_customizeMC 
+
+#call to customisation function nanoAOD_customizeMC imported from PhysicsTools.NanoAOD.nano_cff
+process = nanoAOD_customizeMC(process)
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
+
+
+# Customisation from command line
+
+process.add_(cms.Service('InitRootHandlers', EnableIMT = cms.untracked.bool(False)))
+process.MessageLogger.cerr.FwkReport.reportEvery = 500
+from PhysicsTools.NanoAOD.custom_jme_cff import PrepJMECustomNanoAOD_MC; PrepJMECustomNanoAOD_MC(process)
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
---
+++ crab/helpers.py
@@ -0,0 +1,49 @@
+def GetSampleList(file):
+  samplelist = file.readlines()
+  samplelist = [x.strip() for x in samplelist] 
+  samplelist = [x for x in samplelist if x] # Choose lines that are not empty
+  samplelist = [x for x in samplelist if not(x.startswith("#"))] # Choose lines that do not start with #
+  return samplelist
+
+def TrimPrimaryNameForMC(dataset):
+  name = dataset.split('/')[1]
+  name = name.replace("_13TeV","")
+  name = name.replace("_13p6TeV","")
+  name = name.replace("_TuneCP5","")
+  name = name.replace("pythia","py")
+  name = name.replace("herwig","hw")
+  name = name.replace("madgraph","mg")
+  name = name.replace("powheg","phg")
+  name = name.replace("amcatnlo","mcNLO")
+  name = name.replace("powhegMiNNLO","pwhg")
+  name = name.replace("-photos","")
+  name = name.replace("FXFX","")
+  name = name.replace("_MatchEWPDG20","")
+  return name
+
+def TrimSecondaryNameForMC(dataset):
+  name = dataset.split('/')[2]
+
+  name = name.replace("Run3Winter22MiniAOD-","MC22_122")#RENAME CAMPAIGN. CHECK ITS UPDATED
+  name = name.replace("122X_mcRun3_2021_realistic_v9","") #REMOVE GT. CHECK ITS UPDATED
+
+  name = name.replace("-v1","")# 
+  name = name.replace("-v2","")# 
+  name = name.replace("-v3","")#
+  name = name.replace("-v4","")# Remove any version indication.There should only be one valid version for MC samples
+  return name
+
+def TrimSecondaryNameForData(dataset):
+  name = dataset.split('/')[2]
+  name = name.replace("-v1","")# 
+  name = name.replace("-v2","")# 
+  name = name.replace("-v3","")#
+  name = name.replace("-v4","")# Remove any version indication.
+  return name
+
+def IsSampleData(dataset):
+  name = dataset.split('/')[2]
+  isData = False
+  if   "Run2022" in name:isData = True
+  return isData
+
